eb92ecf6b7d0f57672b4a55a79169af4c411fe4f
MestrieEsteban <esteban94.em@gmail.com>
2021-05-26 22:25:59 +0200

    ok

--- src/mixins/socket.js
+++ src/mixins/socket.js
@@ -3,7 +3,7 @@ import io from 'socket.io-client'
 export default {
   methods: {
     setSocket() {
-      this.socket = io('http://localhost:3000/')
+      this.socket = io('https://swapmanback.herokuapp.com')
     }
   },
 }
